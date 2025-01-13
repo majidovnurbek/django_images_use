959a5e1e5898b5b18d181d489ce952ee3b5a61c3
Nurbek <152709750+majidovnurbek@users.noreply.github.com>
2025-01-13 10:53:14 +0500

    Commit

--- root/post/models.py
+++ root/post/models.py
@@ -4,7 +4,7 @@ class Posts(models.Model):
     title = models.CharField(max_length=100)
     content = models.TextField()
     image = models.ImageField(upload_to='images/%Y/%m/%d')
-
+    
 
     def __str__(self):
         return self.title
